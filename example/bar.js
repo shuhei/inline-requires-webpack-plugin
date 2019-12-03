541babac6a0dcbf5f05e63e7fae8f306de9c219a
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2019-12-03 15:16:52 +0900

    Add a unused export to test tree shaking

--- example/bar.js
+++ example/bar.js
@@ -3,3 +3,7 @@ import baz from "./baz.json";
 export function bar() {
   console.log("bar", baz);
 }
+
+export function unused() {
+  console.log("This function should be removed by tree shaking");
+}
